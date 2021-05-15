3eeb7f638ca85e7636d5129d0c6c7de2a54faeb7
Viktor Novikov <thesuperpothead@gmail.com>
2021-05-15 13:44:10 +0300

    Update scripts.js
    Rotate landscape automatically

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,9 @@
-Array.from(document.querySelectorAll('.vil-player')).map(p => new Plyr(p));
+Array.from(document.querySelectorAll('.vil-player')).map(p => {
+	player = new Plyr(p);
+	player.on('enterfullscreen', event => {
+		screen.orientation.lock('landscape');
+	});
+	  player.on('exitfullscreen', event => {
+		screen.orientation.unlock();
+	});
+});
